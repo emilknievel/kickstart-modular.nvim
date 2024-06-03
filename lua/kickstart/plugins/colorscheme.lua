d02a1c509ed3f7cc6acccf5a11785efbeab760fe
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-06-03 19:02:20 +0200

    generic plugin name for colorschemes

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -10,11 +10,28 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  --   opt = {
+  --     flavour = 'auto',
+  --     background = {
+  --       light = 'latte',
+  --       dark = 'mocha',
+  --     },
+  --     transparent_background = false,
+  --     show_end_of_buffer = true,
+  --   },
+  -- },
 }
 -- vim: ts=2 sts=2 sw=2 et
