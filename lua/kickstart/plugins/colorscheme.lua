2817800a61d5888a8639bb7acd1e79f86aa464cf
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-06-04 17:26:14 +0200

    more colorscheme options

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -10,28 +10,61 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight'
+      -- vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
+    end,
+    opts = {
+      style = 'night',
+      light_style = 'day',
+      transparent = false,
+    },
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    init = function()
+      -- vim.cmd.colorscheme 'catppuccin'
+    end,
+    opts = {
+      flavour = 'auto',
+      background = {
+        light = 'latte',
+        dark = 'mocha',
+      },
+      transparent_background = true,
+      show_end_of_buffer = true,
+    },
+  },
+  {
+    'slugbyte/lackluster.nvim',
+    lazy = false,
+    priority = 1000,
+    init = function()
+      -- vim.cmd.colorscheme 'lackluster'
+      -- vim.cmd.colorscheme 'lackluster-hack'
+      -- vim.cmd.colorscheme 'lackluster-mint'
+      -- vim.cmd.colorscheme 'lackluster-dark'
     end,
   },
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.colorscheme 'catppuccin'
-  --   end,
-  --   opt = {
-  --     flavour = 'auto',
-  --     background = {
-  --       light = 'latte',
-  --       dark = 'mocha',
-  --     },
-  --     transparent_background = false,
-  --     show_end_of_buffer = true,
-  --   },
-  -- },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'rose-pine'
+    end,
+    opts = {
+      dim_inactive_windows = true,
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = false,
+      },
+    },
+  },
 }
+
 -- vim: ts=2 sts=2 sw=2 et
