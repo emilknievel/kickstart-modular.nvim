eba0d30aadb852bbf49f53db1f4db657d2bb8276
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-06-06 17:14:04 +0200

    solarized-osaka theme

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,70 +1,13 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'craftzdog/solarized-osaka.nvim',
     lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'solarized-osaka'
     end,
-    opts = {
-      dim_inactive_windows = true,
-      styles = {
-        bold = true,
-        italic = false,
-        transparency = true,
-      },
-    },
+    opts = {},
   },
-  -- {
-  --   'slugbyte/lackluster.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   init = function()
-  --     -- vim.cmd.colorscheme 'lackluster'
-  --     -- vim.cmd.colorscheme 'lackluster-hack'
-  --     -- vim.cmd.colorscheme 'lackluster-mint'
-  --     -- vim.cmd.colorscheme 'lackluster-dark'
-  --   end,
-  -- },
-  -- {
-  --   'rose-pine/neovim',
-  --   name = 'rose-pine',
-  --   lazy = false,
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.colorscheme 'rose-pine'
-  --   end,
-  --   opts = {
-  --     dim_inactive_windows = true,
-  --     styles = {
-  --       bold = true,
-  --       italic = true,
-  --       transparency = false,
-  --     },
-  --   },
-  -- },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
