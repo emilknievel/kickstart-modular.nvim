4957d2acf0ae03d1bd75b8e4ba9f11e4c68a3ec8
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-06-15 20:55:47 +0200

    Vague theme
    Vague theme is a variant of Rosé Pine.

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,13 +1,24 @@
 return {
   {
-    'craftzdog/solarized-osaka.nvim',
-    lazy = false,
-    priority = 1000,
+    "vague2k/vague.nvim",
     init = function()
-      vim.cmd.colorscheme 'solarized-osaka'
+      vim.cmd.colorscheme 'vague'
     end,
-    opts = {},
+    opts = {
+      transparent = false,
+      bold = false,
+      italic = false,
+    },
   },
+  -- {
+  --   'craftzdog/solarized-osaka.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'solarized-osaka'
+  --   end,
+  --   opts = {},
+  -- },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
